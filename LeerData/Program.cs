ec9ad343a08310a872efafca2168fcb5c22b420a
Lucas Genovez <67564811+NetValueLab@users.noreply.github.com>
2020-09-14 23:05:17 -0300

    Creacion de Solucion y Intalacion de liberias de ASP.NET Core Entity Framework

---
+++ LeerData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Alunos al curso ASP.NET CORE - React Hooks");
+        }
+    }
+} 
